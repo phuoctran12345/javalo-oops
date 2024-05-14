f26f813bb9fb34e2abc1bda659d677b6beafa2ef
Phuocth.dev <134176804+phuoctran12345@users.noreply.github.com>
2024-05-14 21:03:57 +0700

    Update Testlap4.java

--- src/Lab01/Testlap4.java
+++ src/Lab01/Testlap4.java
@@ -102,7 +102,4 @@ public class Testlap4 {
     }
 
 }
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
